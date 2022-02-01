8b50f5485bd7c0ac349f17117cc1ae158efe4c2b
romain-vitalis <romain.vitalis@laplateforme.iO>
2022-02-01 12:01:08 +0100

    blog

--- admin.php
+++ admin.php
@@ -1,174 +1,203 @@
- <?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
 
-session_start();
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>
+        <?php
+          
+            
+            $bdd = mysqli_connect("localhost","root","","blog");
 
 
-    $bdd = mysqli_connect("localhost", "root", "", "blog");
-
-
-    $req = mysqli_query($bdd, "SELECT * FROM utilisateurs INNER JOIN droits ON utilisateurs.id_droits = droits.id");
-
-
-    $res = mysqli_fetch_all($req);
-    var_dump($res);
-
-
-    if (isset($_POST['env'])) {
-        $id1 = $_POST['id'];
-        $login1 = $_POST['login'];
-        $prenom1 = $_POST['prenom'];
-        $email1 = $_POST['email'];
-        if ($_POST['statut'] == "utilisateur") {
-            $id_droit = 1;
-        } elseif ($_POST['statut'] == "administrateur") {
-            $id_droit = 1337;
-        } elseif ($_POST['statut'] == "moderateur") {
-            $id_droit = 42;
-        }
-        $requser = mysqli_query($bdd, "UPDATE utilisateurs SET login='$login1', prenom='$prenom1', email='$email1', id_droits='$id_droit' WHERE  id = $id1 ");
-        header("Location: admin.php");
+            $req= mysqli_query($bdd," SELECT utilisateurs.id, utilisateurs.login, utilisateurs.prenom, utilisateurs.password, utilisateurs.email, droits.nom FROM utilisateurs INNER JOIN droits ON utilisateurs.id_droits = droits.id"); 
+           
+            if(isset($_POST['ban'])){
+                $id1 = $_POST['id'];
         
-    }
+                $reqban = mysqli_query($bdd,"DELETE FROM utilisateurs WHERE id = $id1");
+                var_dump($reqban);
+            }
+        
+        
+            $res= mysqli_fetch_all($req); 
+           
+            $id= $res[0][0];
+            $login= $res[0][1];
+            $email= $res[0][3];
+            $prenom= $res[0][2];
 
-    ?>
+            if (isset($_POST['env'])) {
+                $id1=$_POST['id'];
+                $login1 = $_POST['login'];
+                $prenom1 = $_POST['prenom'];
+                $email1 = $_POST['email'];
+                if($_POST['statut']=="utilisateur"){
+                    $id_droit=1;
+                }
+                elseif($_POST['statut']=="administrateur"){
+                    $id_droit=1337;
+                }
+                elseif($_POST['statut']=="moderateur"){
+                    $id_droit=42;
+                }
 
- <!DOCTYPE html>
- <html>
+                   
+                 $req2= mysqli_query($bdd,"UPDATE utilisateurs SET login='$login1', prenom='$prenom1', email='$email1', id_droits='$id_droit' WHERE  id = $id1 ");
+               header("Location: admin.php");
+               }
 
- <head>
-     <title>Cours PHP / MySQL</title>
-     <meta charset="utf-8">
-
- </head>
-
- <body>
-     <h1>Bases de données MySQL</h1>
-
-     <form action="#" method="post">
+            ?>
+            <form action="#" method="post">
+            
 
 
 
-
-     </form>
-
-     <h1>Info utilisateur</h1>
-     <table>
-         <thead>
-             <th>id</th>
-             <th>login</th>
-             <th>prenom</th>
-             <th>email</th>
-             <th>rôle</th>
-         </thead>
-         <tbody>
-             <?php
-
-                foreach ($res as $utilisateur) {
-                    echo '<tr><form method="post" action="">
-                    <td> <input type="text" disabled="disabled"  value="' . $utilisateur[0] . '" name="id"></td>
-                    <td> <input type="text" value="' . $utilisateur[1] . '" name="login"> </td>
-                    <td> <input type="text" value="' . $utilisateur[2] . '" name="prenom"> </td>
-                    <td> <input type="text" value="' . $utilisateur[4] . '" name="email"> </td>
+            </form>
+ 
+        <h1>Informations du profil</h1>
+        <table>
+            <thead>
+                <th>id</th>
+                <th>login</th>
+                <th>prenom</th>
+                <th>email</th>
+                <th>rôle</th>
+            </thead>
+           <tbody>
+               <?php 
+              
+               foreach ($res as $utilisateur) {
+                   echo '<tr><form method="post" action="">
+                    <td> <input type="text"  value="'.$utilisateur[0].'" name="id"></td>
+                    <td> <input type="text" value="'.$utilisateur[1].'" name="login"> </td>
+                    <td> <input type="text" value="'.$utilisateur[2].'" name="prenom"> </td>
+                    <td> <input type="text" value="'.$utilisateur[3].'" name="email"> </td>
                     <td> <select name="statut" >
                     <option name="uti" value="utilisateur">utilisateur</option>
                     <option name="modo" value="moderateur">moderateur</option>
                     <option name="administrateur" value="administrateur">administrateur</option>
                </select></td><
                <td>   <input type="submit" name="env"  Envoyer /> </td>
-               
+               <td>   <input type="submit"  name=ban value="Bannir"/> </td>
                     </form> </tr>';
-                }
+                    
+               }
 
+               
+               ?>
+            </tbody>
+        </table>
+    </body>
+</html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
 
-                ?>
-         </tbody>
-     </table>
-
-     <?php
-        $req2 = mysqli_query($bdd, "SELECT * FROM articles INNER JOIN utilisateurs ON utilisateurs.id = articles.id_utilisateur ");
-        $res2 = mysqli_fetch_all($req2, MYSQLI_ASSOC);
-        var_dump($res2);
-
-        if (isset($_POST['envarticle'])) {
-            $idarticle = $res2[0]['id'];
-            $article = $_POST['article'];
-            $titre = $_POST['titre'];
-
-
-
-
-            $reqarticle = mysqli_query($bdd, "UPDATE articles SET login='', article='$idarticle', titre='$titre',  WHERE  id = $idarticle ");
-            header("Location: admin.php");
-            var_dump($reqarticle);
-
-        //    if (isset($_POST['delete'])) {
-            // $reqdelete = mysqli_query($bdd, "DELETE FROM articles WHERE id = $idarticle");
-        }    
-        // }
-        ?>
-     <h1>Info article</h1>
-     <table>
-         <thead>
-             <th>titre</th>
-             <th>articles</th>
-             <th>Auter</th>
-         </thead>
-         <tbody>
-             <?php
-
-                foreach ($res2 as $articles) {
-                    echo '<tr><form method="post" action="">
-               <td> <input type="titre"  value="' . $articles['titre'] . '" name="id"></td>
-               <td> <input type="article"  value="' . $articles['article'] . '" name="id"></td>
-               <td> <input type="text" value="' . $articles['login'] . '" name="login"> </td>
-               <td>   <input type="submit" name="envarticle"  Envoyer /> </td>
-               </form> </tr>';
-                }
-
-
-                ?>
-         </tbody>
-     </table>
-
-     </tbody>
-     </table>
-
-     <?php
-        $req3 = mysqli_query($bdd, "SELECT * FROM categories");
-        $res3 = mysqli_fetch_all($req3, MYSQLI_ASSOC);
-
-        if (isset($_POST['envcategorie'])) {
-            $idcategorie = $res3[0]['id'];
-            $nomcategorie = $_POST['nomcategorie'];
-
-
-
-
-            $reqcategorie = mysqli_query($bdd, "UPDATE categories SET nom='$nomcategorie', WHERE  id = $idcategorie ");
-            header("Location: admin.php");
-        }
-        ?>
-
-     <h1>categorie</h1>
-     <table>
-         <thead>
-             <th>nom</th>
-         </thead>
-         <tbody>
-             <?php
-
-                foreach ($res3 as $categorie) {
-                    echo '<tr><form method="post" action="">
-               <td> <input type="titre"  value="' . $categorie['nom'] . '" name="id"></td>
-               <td>   <input type="submit" name="envcategorie"  Envoyer /> </td>
+    </head>
+    <body>
+        <h1>Articles</h1>
+        <?php
           
-               </form> </tr>';
-                }
+            
+            $bdd = mysqli_connect("localhost","root","","blog");
+ 
+
+        //    jai récup les infos d'article avec ma req
+            $req= mysqli_query($bdd,"SELECT articles.id, articles.titre,articles.article,articles.id_categorie,articles.date,utilisateurs.login FROM articles INNER JOIN utilisateurs ON utilisateurs.id = articles.id_utilisateur"); 
+           
+
+            $res= mysqli_fetch_all($req, MYSQLI_ASSOC); 
+            $id= $res[0]['id'];
+            $login= $res[0]['login'];
+            $titre= $res[0]['titre'];
+            $article= $res[0]['article'];
 
 
-                ?>
-         </tbody>
-     </table>
- </body>
+            // Un if pour que quand les infos sont def, qu'elle puisse update les infos dans la bdd
+            if (isset($_POST['envarticle'])) {
+                $titre1=$_POST['titre'];
+                $article1 = $_POST['article'];
+                $id = $_POST['id'];
 
- </html>
+                   
+                   $req= ("UPDATE articles SET titre= '$titre1' , article= '$article1' WHERE  id = $id");
+                   var_dump($req);
+                   $req2= mysqli_query($bdd,$req);
+               }
+
+            ?>
+            <form action="#" method="post">
+            
+
+
+
+            </form>
+ 
+        <h1>Tableau</h1>
+        <table>
+            <thead>
+                <th>ID <a</th>
+                <th>Titre</th>
+                <th>Article</th>
+                <th>login</th>
+
+            </thead>
+           <tbody>
+               <?php 
+            //   Foreach pour afficher les articles
+              foreach ($res as $articles) {
+                echo '<tr><form method="post" action="">
+           <td> <input type="titre"  value="' . $articles['id'] . '" name="id"></td>
+           <td> <input type="titre"  value="' . $articles['titre'] . '" name="titre"></td>
+           <td> <input type="article"  value="' . $articles['article'] . '" name="article"></td>
+           <td> <input type="text" value="' . $articles['login'] . '" name="login"> </td>
+           <td>   <input type="submit" name="envarticle"  Envoyer /> </td>
+           </form> </tr>';
+            }
+
+               
+               ?>
+            </tbody>
+        </table>
+    </body>
+
+    <?php
+
+$req3 = mysqli_query($bdd, "SELECT * FROM categories");
+$res3 = mysqli_fetch_all($req3, MYSQLI_ASSOC);
+
+if (isset($_POST['envoi'])) {
+    $idcategorie = $res3[0]['id'];
+    $nomcategorie = $_POST['nomcategorie'];
+    $reqcategorie = mysqli_query($bdd,"UPDATE categories SET nom='$nomcategorie', WHERE  id = $idcategorie ");
+    
+}
+?>
+
+<h1>categorie</h1>
+<table>
+ <thead>
+     <th>nom</th>
+ </thead>
+ <tbody>
+     <?php
+
+        foreach ($res3 as $categorie) {
+            echo '<tr><form method="post" action="">
+       <td> <input type="titre"  value="' . $categorie['nom'] . '" name="nomcategorie"></td>
+       <td>   <input type="submit" name="envoi" value=Modifier> </td>
+       </form> </tr>';
+        }
+
+        ?>
+ </tbody>
+</table>
+</body>
+
+</html>
